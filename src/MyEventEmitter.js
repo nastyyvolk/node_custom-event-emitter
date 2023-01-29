da7c2b44014f20013be1a4d85a12b30e9e2ab349
Nastya <anastasia.zasiadovk@ivvysoft.com>
2023-01-29 19:43:24 +0200

    solution

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,12 +1,58 @@
 'use strict';
 
-export class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
-}
+module.exports = class MyEventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  onceWrapper(evName, callback) {
+    const wrapped = (...args) => {
+      callback(args);
+      this.off(evName, wrapped);
+    };
+
+    return wrapped;
+  }
+
+  on(evName, callback) {
+    if (!this.events[evName]) {
+      this.events[evName] = [];
+    }
+    this.events[evName].push(callback);
+  }
+
+  once(evName, callback) {
+    this.on(evName, this.onceWrapper(evName, callback));
+  }
+
+  off(evName, callback) {
+    this.events[evName] = this.events[evName]
+      .filter((func) => func !== callback);
+  }
+
+  emit(evName, ...args) {
+    this.events[evName].forEach((callback) => {
+      callback(args);
+    });
+  }
+
+  prependListener(evName, callback) {
+    if (!this.events[evName]) {
+      this.events[evName] = [];
+    }
+
+    this.events[evName].unshift(callback);
+  }
+
+  prependOnceListener(evName, callback) {
+    this.prependListener(evName, this.onceWrapper(evName, callback));
+  }
+
+  removeAllListeners(evName) {
+    this.events[evName] = [];
+  }
+
+  listenerCount(evName) {
+    return this.events[evName] && this.events[evName].length;
+  }
+};
